d9cfa6db45da013a57001ef7b44f171462f20a96
Robert Krajewski <robert.w.krajewski@googlemail.com>
2023-01-21 19:49:51 +0100

    Start refactoring symobl

--- deepaerialmapper/mapping/symbol.py
+++ deepaerialmapper/mapping/symbol.py
@@ -3,14 +3,85 @@ import os
 from dataclasses import dataclass
 from typing import List, Optional
 
+import albumentations as A
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from albumentations.pytorch import ToTensorV2
 from loguru import logger
 
-from deepaerialmapper.classification.symbol import Net, predict
 from deepaerialmapper.mapping.semantic_mask import SemanticClass, SemanticMask
+
+
+class Net(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=in_ch, out_channels=16, kernel_size=5, stride=2
+        )
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(
+            2304, 256
+        )  # First channel size varies when you change parameter
+        self.fc2 = nn.Linear(256, out_ch)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = self.dropout1(x)
+        x = F.max_pool2d(x, 2)
+        x = F.relu(x)
+
+        x = self.conv3(x)
+        x = self.dropout1(x)
+        x = F.max_pool2d(x, 2)
+        x = F.relu(x)
+
+        x = torch.flatten(x, 1)
+
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.dropout2(x)
+        x = self.fc2(x)
+
+        output = F.log_softmax(x, dim=1)
+        return output
+
+
+def predict(
+    model,
+    image: np.ndarray,
+    targets: list,
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+):
+    test_transforms = A.Compose(
+        [
+            A.Resize(height=68, width=68, p=1),
+            ToTensorV2(),
+        ]
+    )
+    model.eval()
+    if device.type == "cuda":
+        model.cuda()
+
+    with torch.no_grad():
+        image_t = test_transforms(image=image)["image"]
+        inp = torch.unsqueeze(image_t, 0)
+        inp = inp.to(device, dtype=torch.float)
+        output = model.forward(inp)
+        index = output.data.cpu().numpy().argmax()
+        return targets[index]
 
 
 @dataclass
